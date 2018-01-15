7c2c5707239f7ba6cffd018135e787e1312ee6ee
tedklin <tedklin@gmail.com>
2018-01-15 09:28:46 -0800

    unit testing without any unit testing

--- test/com/team687/frc2018/commands/superstructure/SuperstructureTest.java
+++ test/com/team687/frc2018/commands/superstructure/SuperstructureTest.java
@@ -1,5 +1,8 @@
 package com.team687.frc2018.commands.superstructure;
 
+import org.junit.Test;
+
+import com.team687.frc2018.constants.SuperstructureConstants;
 import com.team687.frc2018.subsystems.ArmSimulation;
 import com.team687.frc2018.subsystems.WristSimulation;
 
@@ -8,25 +11,45 @@ public class SuperstructureTest {
     public static ArmSimulation m_armSimulation = new ArmSimulation();
     public static WristSimulation m_wristSimulation = new WristSimulation();
 
+    @Test
     public void stowSuperstructure() {
+	System.out.println("StowSuperstructure");
+	m_wristSimulation.setPosition(SuperstructureConstants.kWristStowPos);
+	m_armSimulation.setPosition(SuperstructureConstants.kArmDownPos);
+	System.out.println("Wrist Position: " + m_wristSimulation.getPosition());
+	System.out.println("Arm Position: " + m_armSimulation.getPosition());
+	System.out.println();
     }
 
+    @Test
     public void setIntakePosition() {
+	stowSuperstructure();
+	System.out.println("SetIntakePosition");
+	m_wristSimulation.setPosition(SuperstructureConstants.kWristIntakePos);
+	System.out.println("Wrist Position: " + m_wristSimulation.getPosition());
+	System.out.println("Arm Position: " + m_armSimulation.getPosition());
+	System.out.println();
     }
 
-    public void setSwitchPosition() {
-    }
-
+    @Test
     public void setScalePositionForward() {
+	stowSuperstructure();
+	System.out.println("SetScalePositionForward");
+	m_armSimulation.setPosition(SuperstructureConstants.kArmScaleMidPos);
+	m_wristSimulation.setPosition(SuperstructureConstants.kWristScaleMidPos);
+	System.out.println("Wrist Position: " + m_wristSimulation.getPosition());
+	System.out.println("Arm Position: " + m_armSimulation.getPosition());
+	System.out.println();
     }
 
+    @Test
     public void setScalePositionBackward() {
-    }
-
-    public void adjustScalePositionBottom() {
-    }
-
-    public void adjustScalePositionTop() {
+	stowSuperstructure();
+	System.out.println("SetScalePositionBackward");
+	m_armSimulation.setPosition(SuperstructureConstants.kArmScaleBackwardPos);
+	m_wristSimulation.setPosition(SuperstructureConstants.kWristScaleBackwardPos);
+	System.out.println("Wrist Position: " + m_wristSimulation.getPosition());
+	System.out.println("Arm Posiiton: " + m_armSimulation.getPosition());
     }
 
 }
