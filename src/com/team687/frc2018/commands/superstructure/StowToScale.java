e6709491adbd78bcd3876c927ad468aebf76bc11
tedklin <tedklin@gmail.com>
2018-02-10 23:40:47 -0800

    superstructure intake and backwards scale

---
+++ src/com/team687/frc2018/commands/superstructure/StowToScale.java
@@ -0,0 +1,47 @@
+package com.team687.frc2018.commands.superstructure;
+
+import com.team687.frc2018.Robot;
+import com.team687.frc2018.constants.SuperstructureConstants;
+
+import edu.wpi.first.wpilibj.command.Command;
+import edu.wpi.first.wpilibj.smartdashboard.SmartDashboard;
+
+public class StowToScale extends Command {
+
+    public StowToScale() {
+	requires(Robot.arm);
+	requires(Robot.wrist);
+	requires(Robot.intake);
+    }
+
+    @Override
+    protected void initialize() {
+	SmartDashboard.putString("Current Command", "ScaleScorePosition");
+    }
+
+    @Override
+    protected void execute() {
+	Robot.intake.setRollerPower(-0.3); // hold cube in place as we go up
+	Robot.arm.setPosition(SuperstructureConstants.kArmAutoScaleScorePos);
+	if (Robot.arm.getPosition() > SuperstructureConstants.kArmWristSafePos) {
+	    Robot.wrist.setPosition(SuperstructureConstants.kWristScoreScalePos);
+	} else {
+	    Robot.wrist.setPosition(SuperstructureConstants.kWristStowOffsetPos);
+	}
+    }
+
+    @Override
+    protected boolean isFinished() {
+	return false;
+    }
+
+    @Override
+    protected void end() {
+    }
+
+    @Override
+    protected void interrupted() {
+	end();
+    }
+
+}
--- src/com/team687/frc2018/constants/SuperstructureConstants.java
+++ src/com/team687/frc2018/constants/SuperstructureConstants.java
@@ -24,14 +24,8 @@ public class SuperstructureConstants {
     public static final int kArmOffsetPos = 682;
     public static final int kArmHorizontalPos = 7000;
     public static final int kArmVerticalPos = 18550;
-
-    // simulation purposes
-    public static final int kArmSwitchPos = 118;
-    public static final int kArmScaleBottomPos = 687;
-    public static final int kArmScaleMidPos = 971;
-    public static final int kArmScaleTopPos = 987;
-    public static final int kArmScaleBackwardPos = 1114;
-    public static final int kArmWristSafePos = 254; // arm position where wrist can still safely point down
+    public static final int kArmAutoScaleScorePos = 14000;
+    public static final int kArmWristSafePos = 10000; // arm position where wrist can start extending (backwards)
 
     public static final int kArmTolerance = 67;
     public static final int kArmForwardSoftLimit = kArmVerticalPos + 409;
@@ -50,16 +44,11 @@ public class SuperstructureConstants {
     public static final int kWristIntakePos = -2700;
     public static final int kWristStowPos = 0;
     public static final int kWristStowOffsetPos = -262;
-
-    public static final int kWristSwitchPos = 148;
-    public static final int kWristScaleTopPos = -2122;
-    public static final int kWristScaleMidPos = -2056;
-    public static final int kWristScaleBottomPos = -2052;
-    public static final int kWristScaleBackwardPos = 125;
+    public static final int kWristScoreScalePos = -2056;
 
     public static final int kWristTolerance = 67;
-    public static final int kWristForwardSoftLimit = kWristStowPos;
-    public static final int kWristReverseSoftLimit = kWristScaleTopPos;
+    public static final int kWristForwardSoftLimit = 0;
+    public static final int kWristReverseSoftLimit = 0;
 
     public static final double kRollerPower = 1;
     public static final int kRollerMaxCurrent = 40;
