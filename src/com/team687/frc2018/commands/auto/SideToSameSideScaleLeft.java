f527f76c0827bb9228aa5d519f18e736b43b1793
tedklin <tedklin@gmail.com>
2018-03-05 16:33:14 -0800

    scale autos

--- src/com/team687/frc2018/commands/auto/SideToSameSideScaleLeft.java
+++ src/com/team687/frc2018/commands/auto/SideToSameSideScaleLeft.java
@@ -1,7 +1,6 @@
 package com.team687.frc2018.commands.auto;
 
 import com.team687.frc2018.commands.drive.DriveAtHeading;
-import com.team687.frc2018.constants.AutoConstants;
 import com.team687.frc2018.utilities.NerdyMath;
 
 import edu.wpi.first.wpilibj.command.CommandGroup;
@@ -9,8 +8,7 @@ import edu.wpi.first.wpilibj.command.CommandGroup;
 public class SideToSameSideScaleLeft extends CommandGroup {
 
     public SideToSameSideScaleLeft() {
-	addSequential(
-		new DriveAtHeading(.2, 180, NerdyMath.inchesToTicks(AutoConstants.kRedStartingWallToPivotPoint), .004));
+	addSequential(new DriveAtHeading(.2, 180, NerdyMath.inchesToTicks(167.8), .004));
 	addSequential(new DriveAtHeading(.2, 180, 10000, .004));
     }
 
--- src/com/team687/frc2018/constants/AutoConstants.java
+++ src/com/team687/frc2018/constants/AutoConstants.java
@@ -5,29 +5,65 @@ import com.team687.frc2018.utilities.NerdyMath;
 
 public class AutoConstants {
 
-    // field measurements (inches)
+    // field measurements (absolute inches)
     // change these
-    public static final double kRedStartingWallToSwitchInches = 140;
+    public static final double kRedStartingWallToSwitchInches = 138.5;
     public static final double kRedCenterToRightSwitchFence = 76.75;
     public static final double kRedCenterToRightSwitchInner = 32.25;
     public static final double kRedCenterToLeftSwitchFence = 76.75;
     public static final double kRedCenterToLeftSwitchInner = 32.25;
+    public static final double kRedRightSwitchLength = 56;
+    public static final double kRedLeftSwitchLength = 56;
+    public static final double kRedRightSwitchToScale = 103.65;
+    public static final double kRedLeftSwitchToScale = 103.65;
+    public static final double kRedRightSideWallToScale = 71.57;
+    public static final double kRedLeftSideWallToScale = 71.57;
 
-    // path parameters (ticks)
-    public static double kRobotOriginX = NerdyMath.inchesToTicks((DriveConstants.kDrivetrainWidth / 2) - 12);
+    // field measurements for origin (absolute inches)
+    // don't change these unless we have time and they aren't far off from expected
+    public static final double kRedExchangeLineToCenterLine = 12;
+    public static final double kRedRightSideWallToAllianceStationEdge = 29.69;
+    public static final double kRedLeftSideWallToAllianceStationEdge = 29.69;
+
+    // robot origins
+    // center means center line is y-axis
+    // right/left means right/left side wall is y-axis
+    public static double kRobotCenterOriginX = NerdyMath
+	    .inchesToTicks((DriveConstants.kDrivetrainWidth / 2) - kRedExchangeLineToCenterLine);
+    public static double kRobotRightOriginX = NerdyMath
+	    .inchesToTicks(-(DriveConstants.kDrivetrainWidth / 2) - kRedRightSideWallToAllianceStationEdge);
+    public static double kRobotLeftOriginX = NerdyMath
+	    .inchesToTicks((DriveConstants.kDrivetrainWidth / 2) + kRedLeftSideWallToAllianceStationEdge);
     public static double kRobotOriginY = NerdyMath.inchesToTicks(DriveConstants.kDrivetrainLength / 2);
+
+    // path parameters
     public static double kRedSwitchRightX = NerdyMath
 	    .inchesToTicks((kRedCenterToRightSwitchFence + kRedCenterToRightSwitchInner) / 2); // duncan is a walrus
     public static double kRedSwitchLeftX = NerdyMath
-	    .inchesToTicks((-kRedCenterToRightSwitchFence + kRedCenterToRightSwitchInner) / 2);
+	    .inchesToTicks((-kRedCenterToRightSwitchFence - kRedCenterToRightSwitchInner) / 2);
     public static double kRedSwitchFrontY = NerdyMath
 	    .inchesToTicks(kRedStartingWallToSwitchInches - DriveConstants.kDrivetrainLength);
+    public static double kRedMidRightY = NerdyMath
+	    .inchesToTicks(kRedStartingWallToSwitchInches + kRedRightSwitchLength);
+    public static double kRedMidLeftY = NerdyMath.inchesToTicks(kRedStartingWallToSwitchInches + kRedLeftSwitchLength);
+    public static double kRedScaleRightX = NerdyMath.inchesToTicks(-kRedRightSideWallToScale);
+    public static double kRedScaleLeftX = NerdyMath.inchesToTicks(kRedLeftSideWallToScale);
+    public static double kRedScaleRightY = NerdyMath
+	    .inchesToTicks(kRedStartingWallToSwitchInches + kRedRightSwitchLength + kRedRightSwitchToScale);
+    public static double kRedScaleLeftY = NerdyMath
+	    .inchesToTicks(kRedStartingWallToSwitchInches + kRedLeftSwitchLength + kRedLeftSwitchToScale);
 
     // Bezier curve paths
-    public static BezierCurve kRedCenterToRightSwitchPath = new BezierCurve(kRobotOriginX, kRobotOriginY, kRobotOriginX,
-	    kRedSwitchFrontY / 2, kRedSwitchRightX, kRedSwitchFrontY / 3, kRedSwitchRightX, kRedSwitchFrontY);
-    public static BezierCurve kRedCenterToLeftSwitchPath = new BezierCurve(kRobotOriginX, kRobotOriginY, kRobotOriginX,
-	    kRedSwitchFrontY / 2, kRedSwitchLeftX, kRedSwitchFrontY / 3, kRedSwitchLeftX, kRedSwitchFrontY);
-    public static BezierCurve kRedCenterToRightMidPath = new BezierCurve(0, 0, 0, 0, 0, 0, 0, 0);
-    public static BezierCurve kRedCenterToLeftMidPath = new BezierCurve(0, 0, 0, 0, 0, 0, 0, 0);
+    public static BezierCurve kRedCenterToRightSwitchPath = new BezierCurve(kRobotCenterOriginX, kRobotOriginY,
+	    kRobotCenterOriginX, kRedSwitchFrontY / 2, kRedSwitchRightX, kRedSwitchFrontY / 3, kRedSwitchRightX,
+	    kRedSwitchFrontY);
+    public static BezierCurve kRedCenterToLeftSwitchPath = new BezierCurve(kRobotCenterOriginX, kRobotOriginY,
+	    kRobotCenterOriginX, kRedSwitchFrontY / 2, kRedSwitchLeftX, kRedSwitchFrontY / 3, kRedSwitchLeftX,
+	    kRedSwitchFrontY);
+    public static BezierCurve kRedRightSameSideScalePath = new BezierCurve(-kRobotRightOriginX, -kRobotOriginY,
+	    -kRobotRightOriginX, -kRedMidRightY, -kRobotRightOriginX, -kRedMidRightY, -kRedScaleRightX,
+	    -kRedScaleRightY);
+    public static BezierCurve kRedLeftSameSideScalePath = new BezierCurve(-kRobotLeftOriginX, -kRobotOriginY,
+	    -kRobotLeftOriginX, -kRedMidLeftY, -kRobotLeftOriginX, -kRedMidLeftY, -kRedScaleLeftX, -kRedScaleLeftY);
+
 }
