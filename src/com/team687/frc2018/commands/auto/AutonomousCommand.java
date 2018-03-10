a369c55a10bc58eca049898756c26b4c91d37d3b
tedklin <tedklin@gmail.com>
2018-03-09 17:34:24 -0800

    auto selector

---
+++ src/com/team687/frc2018/commands/auto/AutonomousCommand.java
@@ -0,0 +1,28 @@
+package com.team687.frc2018.commands.auto;
+
+import edu.wpi.first.wpilibj.command.Command;
+
+/**
+ *
+ */
+public class AutonomousCommand extends Command {
+
+    public AutonomousCommand() {
+    }
+
+    protected void initialize() {
+    }
+
+    protected void execute() {
+    }
+
+    protected boolean isFinished() {
+	return true;
+    }
+
+    protected void end() {
+    }
+
+    protected void interrupted() {
+    }
+}
---
+++ src/com/team687/frc2018/commands/auto/RightToRightScaleAuto.java
@@ -0,0 +1,11 @@
+package com.team687.frc2018.commands.auto;
+
+import edu.wpi.first.wpilibj.command.CommandGroup;
+
+public class RightToRightScaleAuto extends CommandGroup {
+
+    public RightToRightScaleAuto() {
+
+    }
+
+}
