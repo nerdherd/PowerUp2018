1f202f941e6d6d193c13155a71179e080eebc583
tedklin <tedklin@gmail.com>
2018-03-10 11:48:01 -0800

    claw commands

---
+++ src/com/team687/frc2018/commands/superstructure/ClawClose.java
@@ -0,0 +1,38 @@
+package com.team687.frc2018.commands.superstructure;
+
+import com.team687.frc2018.Robot;
+
+import edu.wpi.first.wpilibj.command.Command;
+import edu.wpi.first.wpilibj.smartdashboard.SmartDashboard;
+
+public class ClawClose extends Command {
+
+    public ClawClose() {
+	requires(Robot.intake);
+    }
+
+    @Override
+    protected void initialize() {
+	SmartDashboard.putString("Current Command", "ClawClose");
+	Robot.intake.closeClaw();
+    }
+
+    @Override
+    protected void execute() {
+    }
+
+    @Override
+    protected boolean isFinished() {
+	return true;
+    }
+
+    @Override
+    protected void end() {
+    }
+
+    @Override
+    protected void interrupted() {
+	end();
+    }
+
+}
