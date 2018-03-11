bd89165af5b87d607c8a4130b6d23d02449138c4
tedklin <tedklin@gmail.com>
2018-03-11 14:38:34 -0700

    default stow

--- src/com/team687/frc2018/commands/auto/CenterToLeftSwitchAuto.java
+++ src/com/team687/frc2018/commands/auto/CenterToLeftSwitchAuto.java
@@ -2,7 +2,7 @@ package com.team687.frc2018.commands.auto;
 
 import com.team687.frc2018.commands.drive.DriveBezierPath;
 import com.team687.frc2018.commands.intake.SetIntakeRollerPower;
-import com.team687.frc2018.commands.superstructure.BackwardsScaleToStow;
+import com.team687.frc2018.commands.superstructure.DefaultStow;
 import com.team687.frc2018.constants.AutoConstants;
 
 import edu.wpi.first.wpilibj.command.CommandGroup;
@@ -10,7 +10,7 @@ import edu.wpi.first.wpilibj.command.CommandGroup;
 public class CenterToLeftSwitchAuto extends CommandGroup {
 
     public CenterToLeftSwitchAuto() {
-	addParallel(new BackwardsScaleToStow());
+	addParallel(new DefaultStow());
 	addSequential(new DriveBezierPath(AutoConstants.kRedCenterToLeftSwitchPath, 0.5, 0.008, 0.001, true));
 	addParallel(new SetIntakeRollerPower(0.4));
     }
