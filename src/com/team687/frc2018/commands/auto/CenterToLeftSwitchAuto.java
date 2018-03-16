52907f519c8facbcc8055c630d563b3e79b0bd0f
duncanwedig <duncan.wedig@gmail.com>
2018-03-15 18:51:27 -0700

    Changed center to left switch to straight lines

--- src/com/team687/frc2018/commands/auto/CenterToLeftSwitchAuto.java
+++ src/com/team687/frc2018/commands/auto/CenterToLeftSwitchAuto.java
@@ -1,6 +1,8 @@
 package com.team687.frc2018.commands.auto;
 
 import com.team687.frc2018.commands.drive.DriveBezierPath;
+import com.team687.frc2018.commands.drive.DriveStraightDistance;
+import com.team687.frc2018.commands.drive.TurnToAngle;
 import com.team687.frc2018.commands.intake.SetIntakeRollerPower;
 import com.team687.frc2018.commands.superstructure.DefaultStow;
 import com.team687.frc2018.constants.AutoConstants;
@@ -11,7 +13,16 @@ public class CenterToLeftSwitchAuto extends CommandGroup {
 
     public CenterToLeftSwitchAuto() {
 	addParallel(new DefaultStow());
-	addSequential(new DriveBezierPath(AutoConstants.kRedCenterToLeftSwitchPath, 0.5, 0.008, 0.001, true));
+	
+	addSequential(new DriveStraightDistance(AutoConstants.kRedSwitchFrontY/3, 0, 3));
+	addSequential(new TurnToAngle(-30));
+	addSequential(new DriveStraightDistance(AutoConstants.kRedSwitchLeftX * 2, -30, 3));
+	addSequential(new TurnToAngle(0));
+	addSequential(new DriveStraightDistance(AutoConstants.kRedSwitchFrontY - 
+			AutoConstants.kRedSwitchFrontY / 3 - AutoConstants.kRedSwitchLeftX * Math.sqrt(3),
+			0, 3));
+	
+//	addSequential(new DriveBezierPath(AutoConstants.kRedCenterToLeftSwitchPath, 0.5, 0.008, 0.001, true));
 	addParallel(new SetIntakeRollerPower(0.4));
     }
 }
